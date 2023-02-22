51309b6f1ed2b0243fbb53cd33ce925ce9dc60fb
CaQtiml <lsivakorn@gmail.com>
2023-02-22 15:29:30 +0700

    init project

---
+++ terraform/03-main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_security_group" "cap_aws_sg" {
+  name_prefix = "capdetat-rds-sg-postgresql"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "capdetat_aws_sg"
+  }
+}
+
+resource "aws_db_instance" "capdetat_db" {
+  identifier              = "maindb-capdetat"
+  engine                  = "postgres"
+  engine_version          = "14.2"
+  instance_class          = "db.t3.micro"
+  db_name                 = "capdetat_maindb"
+  username                = var.db_username
+  password                = var.db_password
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  publicly_accessible     = true
+  backup_retention_period = 7
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.cap_aws_sg.id]
+
+  tags = {
+    Name = "capdetat_db"
+  }
+}
